329d64916627974388158505d9a2df51d9e4283b
Joel Eckerson <>
2021-03-06 10:33:41 -0600

    base mode done

--- scripts/client.js
+++ scripts/client.js
@@ -4,6 +4,7 @@ let employees = [];
 
 function onReady(){
     $( '#submitButton' ).on('click', submitEmployee );
+    $( '#salaryTable' ).on('click', '.deleteRowButton', deleteRow );
 }
 
 function submitEmployee(){
@@ -12,28 +13,61 @@ function submitEmployee(){
     let newEmployee = {
         firstName: $('#firstNameInput').val(),
         lastName: $('#lastNameInput').val(),
-        id: $('#idInput').val(),
+        id: $('#idInput').val(),    
         title: $('#titleInput').val(),
         annualSalary: $('#annualSalaryInput').val()
     } // end newEmployee
+    
     employees.push(newEmployee);
     displayEmployee( employees );
     console.log(newEmployee);
+    // set the input values back to nothing
+    $('#firstNameInput').val( '' );
+    $('#lastNameInput').val( '' );
+    $('#idInput').val( '' );
+    $('#titleInput').val( '' );
+    $('#annualSalaryInput').val( '' );
 }
 
 function displayEmployee( alsoEmployees ){
     console.log('in displayEmployee');
-    let table = $( '#salaryTable' );
+    let table = $( '#salaryTableBody' );
     table.empty();
     for (let i = 0; i < alsoEmployees.length; i++) {
         table.append(`<tr>
-        <td>${ alsoEmployees.firstName}</td>
-        <td>${ alsoEmployees.lastName}</td>
-        <td>${ alsoEmployees.id}</td>
-        <td>${ alsoEmployees.title}</td>
-        <td>${ alsoEmployees.annualSalary}</td>
-        <td><button class="deletRowButton">Delete</button></td>
+        <td>${ alsoEmployees[i].firstName}</td> 
+        <td>${ alsoEmployees[i].lastName}</td>
+        <td>${ alsoEmployees[i].id}</td>
+        <td>${ alsoEmployees[i].title}</td>
+        <td>${ alsoEmployees[i].annualSalary}</td>
+        <td><button class="deleteRowButton">Delete</button></td>
         </tr>`);
-        
     }
+
+    deleteRow();
+    calculateSalary( alsoEmployees );
+}
+
+function calculateSalary ( andAlsoEmployees ){
+    // check that we're in calculateSalary
+    console.log( 'in calculateSalary' );
+    // take in employees array
+    // loop through and take the annual salary
+    let salary = 0;
+    for (let i = 0; i < andAlsoEmployees.length; i++) {
+        salary += Number(andAlsoEmployees[i].annualSalary);   
+    }
+    // add and divide it by 12
+    $( '#totalMonthlyOutput' ).empty();
+    $( '#totalMonthlyOutput' ).append(salary/12);
+    if(salary/12 > 20000){
+     $( '#totalMonthlyOutput' ).css("background-color", "red" );
+    }
+    // append it to the DOM
+    // if it's over 20k monthly turn red
+
+}
+
+function deleteRow(){
+    console.log( 'in deleteRow:', $( this ).parent().parent().fadeOut() );
 }
